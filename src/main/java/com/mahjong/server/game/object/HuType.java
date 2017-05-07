8913fcb2b5d950bd40fe8251cc4430b1857706fa
miaomiao.wm <miaomiao.wm@alibaba-inc.com>
2017-05-07 21:34:17 +0800

    add beizhu

--- src/main/java/com/mahjong/server/game/object/HuType.java
+++ src/main/java/com/mahjong/server/game/object/HuType.java
@@ -11,6 +11,11 @@ public interface HuType {
 	 */
 	public boolean canHU(WinInfo winInfo, RuleInfo ruleInfo);
 
+	/**
+	 * 本局玩法的基本胜利的赢发，得到这个之后需要调用起对应的isPiaoHU和maybeQiongHu来判断是否是飘胡以及是否是穷胡
+	 * 
+	 * @return
+	 */
 	public WinType getBaseWinType();
 
 	/**
