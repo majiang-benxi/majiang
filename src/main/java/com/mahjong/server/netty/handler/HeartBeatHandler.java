9d83fe89f93e5f9d9b9f0847481242ad98a6718e
yuanjie <yuanjie@jiuxian.com>
2017-06-08 11:52:53 +0800

    ctx.fireChannelRead(protocolModel);

--- src/main/java/com/mahjong/server/netty/handler/HeartBeatHandler.java
+++ src/main/java/com/mahjong/server/netty/handler/HeartBeatHandler.java
@@ -51,8 +51,9 @@ public class HeartBeatHandler extends SimpleChannelInboundHandler<ProtocolModel>
 				Date now = new Date();
 				ClientSession.sessionHeartBeatTimeMap.put(weixinId, now);
 			}
-			ctx.fireChannelRead(protocolModel);// 此处要加这个不然后续handler无法处理
 		}
+		ctx.fireChannelRead(protocolModel);// 此处要加这个不然后续handler无法处理
+		
 	}
 
 	@Override
