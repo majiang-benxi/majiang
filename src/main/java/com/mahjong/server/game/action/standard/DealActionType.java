10a13e0d2478e2048c122a8b0b4ef2d42bae3dc8
wumiao <wumiao@meituan.com>
2017-07-21 12:54:35 +0800

    fix

--- src/main/java/com/mahjong/server/game/action/standard/DealActionType.java
+++ src/main/java/com/mahjong/server/game/action/standard/DealActionType.java
@@ -71,21 +71,21 @@ public class DealActionType implements ActionType {
 		context.getTable().resetPlayersLastDrawTile(zhuangLocation);
 		context.getTable().setFanhui(table.drawBottom(1).getPai()[0]);// 系统翻出一张会牌
 	    context.getTable().getRemainderTileNum().addAndGet(-54);
-		  // test--------tianhu----begin---
- 		PlayerInfo playerInfo3=	context.getTable().getPlayerByLocation(zhuangLocation);
- 
- 	    playerInfo3.setAliveTiles(new Tile(new byte[]{0x13,0x13,0x17,0x17}));
- 		List<TileGroup> tileGroups3=new ArrayList<TileGroup>();
- 		tileGroups3.add(new TileGroup(TileGroupType.CHI_GROUP, new Tile(new byte[]{0x01, 0x02, 0x03})));
- 		tileGroups3.add(new TileGroup(TileGroupType.PENG_GROUP, new Tile(new byte[]{0x28,0x28,0x28})));
- 		tileGroups3.add(new TileGroup(TileGroupType.PENG_GROUP, new Tile(new byte[]{0x37,0x37,0x37})));
- 
- 		playerInfo3.setTileGroups(tileGroups3);
- 		context.getTable().getPlayerByLocation(zhuangLocation)._getSortAliveTiles()
- 		.addTile(zhuangFirstTile).sort();
- 		context.getTable().setFanhui((byte)0x12);
-  		
-  	    //test--------------end----
+//		  // test--------tianhu----begin---
+// 		PlayerInfo playerInfo3=	context.getTable().getPlayerByLocation(zhuangLocation);
+// 
+// 	    playerInfo3.setAliveTiles(new Tile(new byte[]{0x13,0x13,0x17,0x17}));
+// 		List<TileGroup> tileGroups3=new ArrayList<TileGroup>();
+// 		tileGroups3.add(new TileGroup(TileGroupType.CHI_GROUP, new Tile(new byte[]{0x01, 0x02, 0x03})));
+// 		tileGroups3.add(new TileGroup(TileGroupType.PENG_GROUP, new Tile(new byte[]{0x28,0x28,0x28})));
+// 		tileGroups3.add(new TileGroup(TileGroupType.PENG_GROUP, new Tile(new byte[]{0x37,0x37,0x37})));
+// 
+// 		playerInfo3.setTileGroups(tileGroups3);
+// 		context.getTable().getPlayerByLocation(zhuangLocation)._getSortAliveTiles()
+// 		.addTile(zhuangFirstTile).sort();
+// 		context.getTable().setFanhui((byte)0x12);
+//  		
+//  	    //test--------------end----
 		 
 		 		
 		context.getTable().printAllPlayTiles();
